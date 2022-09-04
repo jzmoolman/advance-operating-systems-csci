4f1d564b6cac4608c21993387462586a8818aca0
JZ Moolman <jzmoolman@scapticom.co.za>
2022-09-04 14:25:41 -0400

    Homework 1 quesint 1 to 3, need some cleanup

--- lab1/main.c
+++ lab1/main.c
@@ -18,6 +18,15 @@ typedef struct sum_arg_s {
   int length;
 } sum_args_t;
 
+typedef struct quicksort_arg_s {
+  int level;
+  int id;
+  long* buffer;
+  int p;
+  int r;
+} quicksort_args_t;
+
+
 void* proc_thread(void* arg) {
   printf("thread id %d running\n", ((thread_args_t*)arg)->id);
   for ( int i = 0; i < ((thread_args_t*)arg)->length; i++ ) {
@@ -94,6 +103,25 @@ long parallel_sum(long* buffer, int length, int thread_cnt) {
   return result;
 }
 
+int partitionsort(long* buffer,int p, int r ) {
+
+  int pvalue = buffer[r];
+  int i = p-1;
+  for (int j = p; j < r; j++) {
+    if ( buffer[j] <= pvalue) {
+      i++;
+      int tmp = buffer[i];
+      buffer[i] = buffer[j];
+      buffer[j] = tmp;
+
+    }
+  }
+  int tmp = buffer[i+1];
+  buffer[i+1] = buffer[r];
+  buffer[r] = tmp;
+  return  i+1;
+}
+
 void print_buffer(long* buffer, int length) {
   printf("Buffer = [");
   for ( int i = 0; i < length; i++) {
@@ -102,21 +130,56 @@ void print_buffer(long* buffer, int length) {
   printf("]\n");
 }
 
+void* quicksort(void* arg) {
+  quicksort_args_t* args = (quicksort_args_t*)arg;
+  printf("thread level %d thread id %d\n", args->level, args->id);
+  if (args->p < args->r) {
+    int q = partitionsort(args->buffer,args-> p, args->r);
+    printf("p = %d r = %d q = %d\n", args->p, args->r, q);
+    quicksort_args_t* largs = malloc(sizeof(quicksort_args_t));
+    largs->level = args->level+1;
+    largs->id = 0; 
+
+    largs->buffer = args->buffer;
+    largs->p = args->p;
+    largs->r = q-1;
+    
+    pthread_t lt;
+    pthread_create(&lt,NULL, quicksort, largs );
+    
+    quicksort_args_t* rargs = malloc(sizeof(quicksort_args_t));
+    rargs->level = args->level+1;
+    rargs->id = 1; 
+    rargs->buffer = args->buffer;
+    rargs->p = q+1;
+    rargs->r = args->r;
+    
+    pthread_t rt;
+    pthread_create(&rt,NULL, quicksort, rargs );
+    
+    pthread_join(lt,NULL);
+    pthread_join(rt, NULL);
+
+    free(largs);
+    free(rargs);
+  }
+}
+
 
 int main(int argc,char** argv) {
   printf("Advance Operating Systems - lab1\n");
-  int length = 10;
+  int length = 100;
   srand(time(NULL));
 
   long *array_a = malloc(sizeof(long) * length);
   for ( int i = 0; i < length; i++ ) {
-      //array_a[i] = rand()/1000;
-      array_a[i] = i;
+      array_a[i] = rand()%100;
+      //array_a[i] = i;
   }
 
   long *array_b = malloc(sizeof(long) * length);
   for ( int i = 0; i < length; i++ ) {
-      //array_b[i] = rand()/1000;
+      //array_b[i] = rand()%1000;
       //array_b[i] = i;
   }
 
@@ -136,7 +199,18 @@ int main(int argc,char** argv) {
 
   printf("Sum Array C %lu\n", sum);
 
+  printf("\n");
+  printf("Quicksort\n");
 
+  quicksort_args_t args;
+  args.level = 0;
+  args.id = 0;
+  args.buffer = array_a;
+  args.p = 0;
+  args.r = length-1;
+
+  quicksort(&args);
+  print_buffer(args.buffer, length);
 
   return 0;
 }
